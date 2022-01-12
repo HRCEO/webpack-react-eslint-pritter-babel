45da97a2f6952a6c72adbf09be0dc1ebf6afbb43
[HRCEO] <[kw8989@naver.com]>
2022-01-12 11:56:04 +0900

    ESLint + Prettier 설치 및 설정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 export default function App() {
-    const JSV = undefined;
-    return (
-        <>
-          <div
-              style={{
-                  backgroundColor: 'black',
-                  fontSize:'100px',
-                  color:'aqua'
-              }}
-          >
-              스타일 적용 테스트
-          </div>
-        </>
-    );
+  const JSV = undefined;
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: 'black',
+          fontSize: '100px',
+          color: 'aqua'
+        }}
+      >
+        스타일 적용 테스트
+      </div>
+    </>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,73 @@
-const path = require('path')
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const port = process.env.PORT || 3000;
 
 module.exports = {
-    entry: {
-        app: path.join(__dirname, 'src', 'index.js'),
+  entry: {
+    app: path.join(__dirname, 'src', 'index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
     },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: "[name].js",
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: port,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_module/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            importLoaders: 1
-                        }
-                    },
-                ]
-            },
+    compress: true,
+    port: port
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_module/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          }
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new webpack.DefinePlugin({
-            VERSION: JSON.stringify(' V.01 '),
-            'api.address': JSON.stringify(' DefinePlugin Test '),
-        }),
-        new HtmlWebpackPlugin({
-            hash: true,
-            template: './src/index.html', // 템플릿 경로를 지정
-            templateParameters: { // 템플릿에 주입할 파라매터 변수 지정
-                env: process.env.NODE_ENV === 'development' ? '(DEV)' : '',
-            },
-            minify: process.env.NODE_ENV === 'production' ? {
-                collapseWhitespace: true, // 빈칸 제거
-                removeComments: true, // 주석 제거
-            } : false,
-        })
+      }
     ]
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(' V.01 '),
+      'api.address': JSON.stringify(' DefinePlugin Test ')
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html', // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === 'development' ? '(DEV)' : ''
+      },
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false
+    })
+  ]
+};
